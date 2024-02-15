b847cbf245afd2c1f22ed1c9ddd0f07ac119a7c5
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2024-02-15 23:21:45 +0900

    command doc

--- cmd/generate.go
+++ cmd/generate.go
@@ -49,14 +49,9 @@ type Config struct {
 
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "generate Terraform template",
+	Short: "Generate Terraform template from your existiong SAKURA Cloud",
+	Long:  `Generate Terraform template from your existiong SAKURA Cloud`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		config := Config{}
